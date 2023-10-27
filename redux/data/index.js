937618c9b98e78f716e1eb9e3acb28c72330db6c
Mohammad Mosavat <42.octmst@gmail.com>
2023-10-27 14:43:56 +0330

    create a data for our product

---
+++ redux/data/index.js
@@ -0,0 +1,32 @@
+export const DataProduct = [
+  {
+    img: "/products/unsplash_2XcbGfYShfk.jpg",
+    cato: "T-Shirt",
+    price: 599,
+    id: 1,
+  },
+  {
+    img: "/products/unsplash_A0mSSCEVh9A.jpg",
+    cato: "T-Shirt",
+    price: 799,
+    id: 2,
+  },
+  {
+    img: "/products/unsplash_fRlsgH_MTNw.jpg",
+    cato: "T-Shirt",
+    price: 299,
+    id: 3,
+  },
+  {
+    img: "/products/unsplash_VW5VjskNXZ8.jpg",
+    cato: "T-Shirt",
+    price: 499,
+    id: 4,
+  },
+  {
+    img: "/products/unsplash_x8Vg7Up6TUc.jpg",
+    cato: "T-Shirt",
+    price: 699,
+    id: 5,
+  },
+];
